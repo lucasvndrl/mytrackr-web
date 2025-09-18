45c2ef5677769b35c51006245063c11de65e15bd
Lucas Vanderlei <lucasvndrl@gmail.com>
2025-09-18 20:33:15 -0300

    posters uuid

--- src/utils/transformMoviesWithPoster.ts
+++ src/utils/transformMoviesWithPoster.ts
@@ -10,23 +10,23 @@ export function transformMoviesWithPoster(movies: any[]): any[] {
 
 const posters = [
   {
-    movieId: "1",
+    movieId: "c3a0dfaa-b21b-4c6a-b6ff-847fab3a5e6c",
     img: "/batman.png",
   },
   {
-    movieId: "2",
+    movieId: "052ce8a1-5409-4e03-81cf-f98651cf515e",
     img: "/dontlookup.png",
   },
   {
-    movieId: "3",
+    movieId: "6f1a2c58-b20a-48cd-a152-881e6693a97c",
     img: "/frenchdispatch.png",
   },
   {
-    movieId: "4",
+    movieId: "a629195e-a702-45f8-92ca-f4daa6c45cc5",
     img: "/nowayhome.png",
   },
   {
-    movieId: "5",
+    movieId: "a9151154-c6ac-42be-8055-53a1d072fc77",
     img: "/thepower.png",
   },
 ];
