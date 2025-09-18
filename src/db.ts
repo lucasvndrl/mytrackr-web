989eb837f091dda99744b86ce77152d85cb0e48c
Lucas Vanderlei <lucasvndrl@gmail.com>
2025-09-18 19:51:00 -0300

    fix pool configuration

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,5 @@
 import { Kysely, PostgresDialect } from "kysely";
-import pg from "pg";
+import pg, { Pool } from "pg";
 import { Database } from "./types/dbTables";
 
 const connectionString =
@@ -8,14 +8,19 @@ const connectionString =
     : process.env.DATABASE_URL) ??
   "postgresql://postgres:1234@localhost:5432/postgres";
 console.log(connectionString);
-const pool = new pg.Pool({
-  connectionString,
-  ssl: connectionString.includes("supabase.co")
-    ? {
-        rejectUnauthorized: false,
-        checkServerIdentity: () => undefined, // Desabilita verificação do servidor
-      }
-    : undefined,
+const pool = new Pool({
+  host: process.env.DATABASE_HOST!,
+  port: parseInt(process.env.DATABASE_PORT!),
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+  max: 3,
+  idleTimeoutMillis: 10_000,
+  connectionTimeoutMillis: 5_000,
+  ssl: {
+    rejectUnauthorized: false,
+    checkServerIdentity: () => undefined,
+  },
 });
 
 export const db = new Kysely<Database>({
