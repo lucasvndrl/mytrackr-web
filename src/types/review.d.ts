9d71f96574d09515caa74731a7ed00a225c9c875
Lucas Vanderlei <lucasvndrl@gmail.com>
2025-09-29 18:28:13 -0300

    logs

--- src/types/review.d.ts
+++ src/types/review.d.ts
@@ -7,6 +7,8 @@ export type Review = {
   movie_poster: ImageSourcePropType;
   movie_title: string;
   reviewer_name: string;
-  reviewer_avatar: ArrayBuffer;
+  reviewer_avatar: SupabaseBuffer;
   review_date?: string;
 };
+
+type SupabaseBuffer = { type: "Buffer"; data: number[] };
